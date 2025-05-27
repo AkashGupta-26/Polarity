670aa5264c7ed3a1a06f8eb4666e88bd1e8c46ae
Akash Gupta <139330968+AkashGupta-26@users.noreply.github.com>
2025-05-28 00:48:11 +0530

    Fixed some formatting

--- search.h
+++ search.h
@@ -9,10 +9,7 @@ struct ScoredMove {
     int score;
 };
 
-// Most Valuable Victim - Least Valuable Aggressor (MVV-LVA) table
-// to avoid runtime calculations we are using a 12x12 array instead of 6x6
-// MVV LVA [attacker][victim]
-
+// Least Valuable Aggressor Most Valuable Victim (MVV-LVA) table
 static int MvvLva[12][12] = {
  	105, 205, 305, 405, 505, 605,  105, 205, 305, 405, 505, 605,
 	104, 204, 304, 404, 504, 604,  104, 204, 304, 404, 504, 604,
@@ -29,40 +26,49 @@ static int MvvLva[12][12] = {
 	100, 200, 300, 400, 500, 600,  100, 200, 300, 400, 500, 600
 };
 
+// Maximum ply depth for search
 const int maxPly = 64;
 
+// Search parameters
+int ply; 
+int searchedNodes; 
+
+// Late Move Reduction (LMR) parameters
+const int FullDepthMoves = 4; // Number of moves to search at full depth
+const int ReductionLimit = 3; // Maximum reduction depth
+
+// Null Move pruning parameters
+const int NullMoveReduction = 2; // Reduction depth for null move pruning
+
 int killerMoves[2][maxPly];
+int historyMoves[12][64];
 
-int historyMoves[12][maxPly];
-
-int pvLength[maxPly]; 
-
-int pvTable[maxPly][maxPly]; // principal variation table
+// Table to store principal variation moves
+int PrincipalVariationLength[maxPly]; 
+int PrincipalVariationTable[maxPly][maxPly];
 
 // follow PV flags
-int followPV, scorePV;
+int followPrincipalVariation, scorePrincipalVariation;
 
-int ply; // half-move counter
-int searchedNodes; // total nodes searched
-
-static inline void enablePVscoring(Board *board, MoveList *list) {
-    
-    followPV = 0;
+// Allows Principal Variation to be evaluated first
+static inline void enablePrincipalVariationScoring(Board *board, MoveList *list) {
+    followPrincipalVariation = 0;
     for (int i = 0; i < list->count; i++){
-        if (pvTable[0][ply] == list->moves[i]) {
-            scorePV = 1;
-            followPV = 1;
+        if (PrincipalVariationTable[0][ply] == list->moves[i]) {
+            scorePrincipalVariation = 1;
+            followPrincipalVariation = 1;
         }
     }
 }
 
+// Assign score to moves for sorting
 static inline int scoreMove(Board *board, int move) {
     int piece = decodePiece(move); 
     int target = decodeTarget(move);
 
-    if (scorePV){
-        if (pvTable[0][ply] == move) {
-            scorePV = 0;
+    if (scorePrincipalVariation){
+        if (PrincipalVariationTable[0][ply] == move) {
+            scorePrincipalVariation = 0;
             return 20000; // Highest score for principal variation moves
         }
     }
@@ -90,6 +96,7 @@ static inline int scoreMove(Board *board, int move) {
     return 0;
 }
 
+// Print move scores for debugging
 void printMoveScores(Board *board, MoveList *list) {
     std::cout << "Move Scores:" << std::endl;
     for (int i = 0; i < list->count; ++i) {
@@ -117,7 +124,7 @@ static inline void sortMoves(Board *board, MoveList *list) {
     }
 }
 
-
+// Quiescence search to handle captures
 static inline int quiescienceSearch(Board *board, int alpha, int beta) {
     // evaluate position
     searchedNodes++;
@@ -134,76 +141,80 @@ static inline int quiescienceSearch(Board *board, int alpha, int beta) {
     generateMoves(board, &moveList);
     sortMoves(board, &moveList); // Sort moves by score
 
-    for (int count = 0; count < moveList.count; ++count) {
-        copyBoard(board); // Backup the current board state
-        ply++;
-        if (makeMove(board, moveList.moves[count], OnlyCaptures) == 0){
-            ply--;
-            continue; // Skip illegal moves
-        }
+    copyBoard(board); // Backup the current board state
 
+    for (int count = 0; count < moveList.count; ++count) {
+        if (makeMove(board, moveList.moves[count], OnlyCaptures) == 0)
+            continue; // Skip illegal moves
+        
+        ply++;
         int score = -quiescienceSearch(board, -beta, -alpha);
 
         ply--;
         takeBack(board, backup); // Restore the board state
         
-        // fail-hard beta cutoff
-        if (score >= beta) {
+        // Fail-hard beta cutoff
+        if (score >= beta) 
             return beta; // Beta cutoff fails high
-        }
 
-        if (score > alpha) {
-            // PV node
-            alpha = score;
-        }
+        if (score > alpha)
+            alpha = score; // Update alpha for PV node
     }
-    // node fails low
-    return alpha;
-}
 
-const int FullDepthMoves = 4;
-const int ReductionLimit = 3;
+    return alpha; // Return the best score found
+}
 
 static inline int negamax(Board *board, int alpha, int beta, int depth){
 
-    //int foundPV = 0;
-    pvLength[ply] = ply;
+    PrincipalVariationLength[ply] = ply;
 
-    if (depth == 0) {
+    if (depth == 0) 
         return quiescienceSearch(board, alpha, beta); // Quiescence search at leaf nodes
-    }
-
-    if (ply > maxPly - 1) {
+    
+    if (ply > maxPly - 1) 
         return evaluate(board); // Return evaluation if maximum ply is reached
-    }
-
-       searchedNodes++;
+    
+    searchedNodes++;
     
     // checks if the king of the side to move is in check
-    int inCheck = isSquareAttacked(board, getLSBindex(board->bitboards[(board->sideToMove == white) ? K : k]), board->sideToMove ^ 1);
+    int inCheck = isBoardInCheck(board);
     
     // increase search depth for checks;
-    if (inCheck) {
+    if (inCheck)
         depth++;
-    }
     
     int legalMoves = 0;
     int movesSearched = 0;
 
+    // Null Move Pruning
+    if (depth >= 3 && !inCheck && ply) {
+        copyBoard(board); 
+        board->sideToMove ^= 1; // Switch sides
+        board->enPassantSquare = noSquare; // Reset en passant square
+        
+        int score = -negamax(board, -beta, -beta + 1, depth - 1 - NullMoveReduction); 
+        takeBack(board, backup); // Restore the board state
+
+        if (score >= beta) 
+            return beta; // Beta cutoff fails high
+        
+    }
+
     MoveList moveList;
     generateMoves(board, &moveList);
 
-    if (followPV) enablePVscoring(board, &moveList); // Enable PV scoring if following principal variation
+    if (followPrincipalVariation) 
+        enablePrincipalVariationScoring(board, &moveList); // Enable PV scoring if following principal variation
 
     sortMoves(board, &moveList); // Sort moves by score
 
+    copyBoard(board); // Backup the current board state
+
     for (int count = 0; count < moveList.count; ++count) {
-        copyBoard(board); // Backup the current board state
-        ply++;
-        if (makeMove(board, moveList.moves[count]) == 0){
-            ply--;
+        if (makeMove(board, moveList.moves[count]) == 0)
             continue; // Skip illegal moves
-        }
+
+        ply++;
         legalMoves++;
         int score;
         
@@ -211,12 +222,12 @@ static inline int negamax(Board *board, int alpha, int beta, int depth){
             score = -negamax(board, -beta, -alpha, depth - 1);
         }
         else{
+            // Late Move Reduction (LMR)
             if(movesSearched >= FullDepthMoves && depth >= ReductionLimit && 
                 !inCheck && !decodeCapture(moveList.moves[count]) && !decodePromoted(moveList.moves[count]))
                     score = -negamax(board, -alpha - 1, -alpha, depth - 2);
             
-            // hack to ensure that full-depth search is done
-            else score = alpha + 1;
+            else score = alpha + 1; // Ensure full-depth search
             
             // if found a better move during LMR
             if(score > alpha)
@@ -229,6 +240,7 @@ static inline int negamax(Board *board, int alpha, int beta, int depth){
                     score = -negamax(board, -beta, -alpha, depth-1);
             }
         }
+
         movesSearched++; //comment this line to disable Late Move Reduction (LMR)
         ply--;
         takeBack(board, backup); // Restore the board state
@@ -241,71 +253,60 @@ static inline int negamax(Board *board, int alpha, int beta, int depth){
             }
             return beta; // Beta cutoff fails high
         }
-
+        
+        // PV node
         if (score > alpha) {
-            // PV node
             if (!decodeCapture(moveList.moves[count])){
                 historyMoves[decodePiece(moveList.moves[count])][decodeTarget(moveList.moves[count])] += depth; // Update history move
             }
-            alpha = score;
-            //foundPV = 1; the entire found PV logic is redundant after implementing the LMR
-
-            pvTable[ply][ply] = moveList.moves[count]; // Store the best move in the principal variation table
+            alpha = score; // Update alpha for PV node
             
-            for (int nextPly = ply + 1; nextPly < pvLength[ply + 1]; nextPly++) {
-                pvTable[ply][nextPly] = pvTable[ply + 1][nextPly]; // Extend the principal variation
+            PrincipalVariationTable[ply][ply] = moveList.moves[count]; // Store the best move in the principal variation table
+            
+            for (int nextPly = ply + 1; nextPly < PrincipalVariationLength[ply + 1]; nextPly++) {
+                PrincipalVariationTable[ply][nextPly] = PrincipalVariationTable[ply + 1][nextPly]; // Extend the principal variation
             }
-            pvLength[ply] = pvLength[ply + 1]; // Update the length of the principal variation
+            PrincipalVariationLength[ply] = PrincipalVariationLength[ply + 1]; // Update the length of the principal variation
         }
     }
 
     if (legalMoves == 0) {
         // If no legal moves, check for checkmate or stalemate
-        if (inCheck) {
+        if (inCheck)
             return -49000 + ply; // Checkmate
-        } else {
+        else
             return 0; // Stalemate
-        }
     }
+
     // node fails low
     return alpha; // Return the best score found
 }
 
 void searchPosition(Board *board, int depth) {
-    /*
-    I actually don't know if I need to reset in case of uci as it 
-    always loads everything from start at each move but yeah I feel like
-    it is better to reset everything here so that I don't have to worry about it later.
-
-    Edit: I was right, it is better to reset everything here, because these tables are 
-    not reset when the position is changed, so if I don't reset them here, the search 
-    will become more and more slow.
-    */
-
     ply = 0;
     searchedNodes = 0;
-    followPV = 0;
-    scorePV = 0;
+    followPrincipalVariation = 0;
+    scorePrincipalVariation = 0;
     
-    memset(pvLength, 0, sizeof(pvLength)); 
-    memset(pvTable, 0, sizeof(pvTable)); 
+    memset(PrincipalVariationLength, 0, sizeof(PrincipalVariationLength)); 
+    memset(PrincipalVariationTable, 0, sizeof(PrincipalVariationTable)); 
     memset(killerMoves, 0, sizeof(killerMoves));
     memset(historyMoves, 0, sizeof(historyMoves));
 
     for (int curDepth = 1; curDepth <= depth; curDepth++){
-        followPV = 1;
+        followPrincipalVariation = 1;
         int score = negamax(board, -50000, 50000, curDepth);
         
         std::cout << "info score cp " << score * ((board->sideToMove == white)? 1 : -1)  << " depth " << curDepth
             << " nodes " << searchedNodes << " pv ";
 
-        for (int i = 0; i <= pvLength[0]; i++) {
-            if (pvTable[0][i] == 0) break; // Stop at the end of the principal variation
-            std::cout << moveToUCI(pvTable[0][i]) << " ";
+        for (int i = 0; i <= PrincipalVariationLength[0]; i++) {
+            if (PrincipalVariationTable[0][i] == 0) break; // Stop at the end of the principal variation
+            std::cout << moveToUCI(PrincipalVariationTable[0][i]) << " ";
         }
         std::cout << std::endl;
     }
-    std::cout << "bestmove " << moveToUCI(pvTable[0][0]) << std::endl;
+    std::cout << "bestmove " << moveToUCI(PrincipalVariationTable[0][0]) << std::endl;
 }
 
 #endif // SEARCH_H;

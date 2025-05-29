c2051d363bff5c81331bbdad0752a465752c40a7
Akash Gupta <139330968+AkashGupta-26@users.noreply.github.com>
2025-05-30 02:11:38 +0530

    V1.8 - Repetition detection, UCI timing controls, search polish

--- engine.cpp
+++ engine.cpp
@@ -6,6 +6,8 @@
 // #include "evaluate.h"
 #include "search.h"
 
+std::ofstream logFile("search_log.txt");
+
 using namespace std;
 
 int parseMove(Board *board, const string &moveStr) {
@@ -60,6 +62,7 @@ void parsePosition(Board *board, const string &input) {
         while (ss >> moveStr) {
             int move = parseMove(board, moveStr);
             if (move) {
+                repetitionTable[repetitionIndex++] = board->zobristHash; // Store the Zobrist hash for repetition detection
                 makeMove(board, move);
             } else {
                 cerr << "Invalid move: " << moveStr << endl;
@@ -68,21 +71,73 @@ void parsePosition(Board *board, const string &input) {
     }
 }
 
+// do gepth 6 wtime 180000 ms btime 180000 ms binc 1000 winc 1000 movetime 1000 movestogo 40
 
-void parseGo(Board *board, const string &input) {
+void parseGo(Board *board, const string &input, SearchUCI *searchParams) {
     // Handles "go depth 10"
-    int depth = 6; // Default depth
+    int depth = -1, movestogo = 30, movetime = -1;
+    int time = 90000, inc = 0; // default time set to 90 seconds
     size_t pos = input.find("depth ");
     if (pos != string::npos) {
         depth = stoi(input.substr(pos + 6));
     }
 
-    //cout << "info depth " << depth << endl;
-    
-    searchPosition(board, depth);
+    size_t wtimePos = input.find("wtime ");
+    size_t btimePos = input.find("btime ");
+    size_t bincPos = input.find("binc ");
+    size_t wincPos = input.find("winc ");
+
+    if (board->sideToMove == white) {
+        if (wtimePos != string::npos) {
+            time = stoi(input.substr(wtimePos + 6));
+        }
+        if (wincPos != string::npos) {
+            inc = stoi(input.substr(bincPos + 5));
+        }
+    } 
+    else {
+        if (btimePos != string::npos) {
+            time = stoi(input.substr(btimePos + 6));
+        }
+        if (bincPos != string::npos) {
+            inc = stoi(input.substr(bincPos + 5));
+        }
+    }
+
+    if (input.find("movetime ") != string::npos) {
+        movetime = stoi(input.substr(input.find("movetime ") + 9));
+    }
+
+    if (input.find("movestogo ") != string::npos) {
+        movestogo = stoi(input.substr(input.find("movestogo ") + 10));
+    }
+
+    if (movetime != -1) {
+        time = movetime; // Use movetime if specified
+        movestogo = 1; // Set movestogo to 1 for single move
+    }
+
+    if (time != -1){
+        searchParams->timedGame = 1;
+        time /= movestogo; // Calculate time per move
+        time -= 50;
+    }
+
+    if (depth == -1){
+        depth = 40;
+    }
+
+    searchParams->depth = depth;
+    searchParams->startTime = TIME_IN_MILLISECONDS;
+    searchParams->stopTime = searchParams->startTime + time;
+    searchParams->increment = inc;
+    searchParams->quit = 0;
+    searchParams->stop = 0;
+
+    searchPosition(board, searchParams);
 }
 
-void uci(Board *board) {
+void uci(Board *board, SearchUCI *searchParams) {
     cout << "id name Polarity" << endl;
     cout << "id author Magnet" << endl;
     cout << "uciok" << endl;
@@ -96,9 +151,12 @@ void uci(Board *board) {
         } else if (input == "isready") {
             cout << "readyok" << endl;
         } else if (input == "quit") {
+            searchParams->quit = 1;
             break;
-        } else if (input.rfind("go", 0) == 0) {
-            parseGo(board, input);
+        } else if (input == "stop") {
+            searchParams->stop = 1;
+        }else if (input.rfind("go", 0) == 0) {
+            parseGo(board, input, searchParams);
         } else if (input.rfind("position", 0) == 0) {
             clearTranspositionTable();
             parsePosition(board, input);
@@ -130,17 +188,20 @@ int main(){
     //cout << "Welcome to Polarity Chess Engine!" << endl;
     initializeAll();
     Board board;
+    SearchUCI searchParams;
+    searchParams.depth = 12; // Default search depth
     int uciMode = 1;
     //MoveList list;
     if (uciMode) {
-        uci(&board);
+        uci(&board, &searchParams);
         return 0; // Exit after UCI initialization
     }
-    parseFEN(&board, repetition_position);
+    parseFEN(&board, "3rr1k1/pb3ppp/4p3/n1p5/1Q5P/P1P5/qBPN1PP1/3RR1K1 w - - 0 21");
     printBoard(&board);
-    searchPosition(&board, 10);
+    searchPosition(&board, &searchParams); // Search with the initial position
 
     makeMove(&board, PrincipalVariationTable[0][0]); // Make the first move from the principal variation
-    searchPosition(&board, 10); // Search again after making the move
+    searchPosition(&board, &searchParams); // Search again after making the move
+    logFile.close();
 
 }
